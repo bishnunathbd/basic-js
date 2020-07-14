f2e069d14c68c3ba9b15932d9463029e3cc6b110
bishnunathbd <bishnunath774@gmail.com>
2020-07-14 10:12:17 +0600

    variable type

---
+++ README.md
@@ -0,0 +1,1 @@
+# basic-js with Programming Hero
---
+++ var-type.js
@@ -0,0 +1,11 @@
+var bananaPrice = 12;
+console.log(typeof bananaPrice);
+
+var name = 'Shuvo';
+console.log(typeof name);
+
+// boolean 
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+console.log(isRich);
