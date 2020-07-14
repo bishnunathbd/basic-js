402a595ec838fb95cb8ccc6232e169961ca8f535
bishnunathbd <bishnunath774@gmail.com>
2020-07-14 11:09:18 +0600

    upper-lower case, indexOf, split

--- var-type.js
+++ var-type.js
@@ -1,11 +1,27 @@
 var bananaPrice = 12;
-console.log(typeof bananaPrice);
+// console.log(typeof bananaPrice);
 
 var name = 'Shuvo';
-console.log(typeof name);
+// console.log(typeof name);
 
 // boolean 
 var isHot = true;
 var isRich = false;
-console.log(typeof isHot);
-console.log(isRich);
+// console.log(typeof isHot);
+// console.log(isRich);
+
+
+//toLowerCase(), indexOf(), split()
+var promise = 'I promise I will work hard to become a programmer.';
+
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+
+// indexOf() - returns the first index at which a given element can be found in the array, 
+// -1 if it is not present.
+console.log(promise.indexOf('will'));
+
+// split() - to split a string into an array of substrings, and returns the new array.
+console.log(promise.split(" "));
+
+
