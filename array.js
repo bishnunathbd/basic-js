ce85e30c31d0c6b0a225bac3dd47e35aac9c847e
bishnunathbd <bishnunath774@gmail.com>
2020-07-14 20:22:19 +0600

    array, index, indexOf

---
+++ array.js
@@ -0,0 +1,15 @@
+var friendsAge = [26, 31, 24, 22, 44];
+
+var tanuAge = friendsAge[3];
+console.log(tanuAge);
+
+console.log(friendsAge);
+// set a value by index of an array
+friendsAge[4] = 14;
+console.log(friendsAge);
+
+// find array element position
+var position = friendsAge.indexOf(24);
+console.log(position);
+var position = friendsAge.indexOf(224);
+console.log(position);
