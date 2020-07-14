1e2ff9f6370ebdb5b487c86d231ee74dfcbf15dc
bishnunathbd <bishnunath774@gmail.com>
2020-07-14 20:47:27 +0600

    array length, push, pop, unshift, shift

--- array.js
+++ array.js
@@ -13,3 +13,24 @@ var position = friendsAge.indexOf(24);
 console.log(position);
 var position = friendsAge.indexOf(224);
 console.log(position);
+
+// find how many elements in an array
+console.log(friendsAge.length);
+
+// add new element to the end of an array
+console.log(friendsAge);
+friendsAge.push(50);
+friendsAge.push(70);
+console.log(friendsAge);
+
+// remove element from the end of an array
+friendsAge.pop();
+console.log(friendsAge);
+
+// add element at the beginning
+friendsAge.unshift(99);
+console.log(friendsAge);
+
+// remove element from the beginning
+friendsAge.shift();
+console.log(friendsAge);
